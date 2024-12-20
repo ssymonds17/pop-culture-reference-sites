fbf70b0248e1536cf96032c1b7362de32d326ec2
Samuel Symonds <samuel.symonds@postcodelottery.co.uk>
2024-12-20 18:11:29 +0000

    Update modal header styling

--- music/apps/music/src/components/Modal/Modal.tsx
+++ music/apps/music/src/components/Modal/Modal.tsx
@@ -9,9 +9,13 @@ export const Modal = ({ isOpen, onClose, type }: ModalProps) => {
 
   return (
     <div className="flex top-0 left-0 absolute justify-center items-center w-screen h-screen bg-gray-400 bg-opacity-75">
-      <div className="flex w-1/4 h-1/4 border-2 bg-opacity-100 bg-white">
-        <button onClick={onClose}>Close</button>
-        <h1>Add {type}</h1>
+      <div className="w-1/2 h-1/2 border-2 bg-opacity-100 bg-white justify-between">
+        <div className="flex justify-between m-2">
+          <h1>Add {type}</h1>
+          <button className="border-2" onClick={onClose}>
+            Close
+          </button>
+        </div>
       </div>
     </div>
   );
