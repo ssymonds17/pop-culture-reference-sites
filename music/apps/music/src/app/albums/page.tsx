9a32c30925c7eca45fef4436d879bed78b282caf
Samuel Symonds <samuel.symonds@postcodelottery.co.uk>
2024-12-20 17:04:32 +0000

    Create albums and songs pages

---
+++ music/apps/music/src/app/albums/page.tsx
@@ -0,0 +1,9 @@
+const AlbumsPage = () => {
+  return (
+    <h1 className="text-4xl font-bold text-center text-gray-900">
+      Albums Page
+    </h1>
+  );
+};
+
+export default AlbumsPage;
---
+++ music/apps/music/src/app/songs/page.tsx
@@ -0,0 +1,7 @@
+const SongsPage = () => {
+  return (
+    <h1 className="text-4xl font-bold text-center text-gray-900">Songs Page</h1>
+  );
+};
+
+export default SongsPage;
