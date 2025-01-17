1f049c60ce78541f878237e1575af7f993ad69d9
Samuel Symonds <samuel.symonds@postcodelottery.co.uk>
2025-01-17 12:21:52 +0000

    Create operation to retrieve artist record by id

--- music/apps/api/src/routes/artists.ts
+++ music/apps/api/src/routes/artists.ts
@@ -1,11 +1,38 @@
 import express from 'express';
+import { ARTISTS_TABLE_NAME, documentClient } from '@music/dynamodb';
+import {
+  GetCommand,
+  GetCommandInput,
+  GetCommandOutput,
+} from '@aws-sdk/lib-dynamodb';
 // Handles requests made to /api/artists
 export const artistsRouter = express.Router();
 
 // Respond to a GET request to the /api/artists route:
-artistsRouter.get('/', (req, res) =>
-  res.send('Got a GET request at /api/artists')
-);
+artistsRouter.get('/:artistId', async (req, res) => {
+  const params: GetCommandInput = {
+    TableName: ARTISTS_TABLE_NAME,
+    Key: {
+      id: req.params.artistId,
+    },
+  };
+  const result: GetCommandOutput = await documentClient.send(
+    new GetCommand(params)
+  );
+
+  if (result.Item) {
+    res.send({
+      message: 'Successfully retrieved artist',
+      status: 200,
+      artist: result.Item,
+    });
+  } else {
+    res.send({
+      message: 'Could not find artist',
+      status: 404,
+    });
+  }
+});
 
 // Respond to a PUT request to the /api/artists route:
 artistsRouter.put('/', (req, res) =>
--- music/libs/dynamodb/src/lib/client.ts
+++ music/libs/dynamodb/src/lib/client.ts
@@ -3,6 +3,4 @@ import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 
 // barebones DynamoDB client
 // https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/Package/-aws-sdk-lib-dynamodb/
-const client = new DynamoDBClient();
-
-export const documentClient = DynamoDBDocumentClient.from(client);
+export const documentClient = DynamoDBDocumentClient.from(new DynamoDBClient());
