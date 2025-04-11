4bcbb0d16879f201d23bb7dcd688fa808b618ff5
Samuel Symonds <samuel.symonds@postcodelottery.co.uk>
2025-04-11 14:32:07 +0100

    Add new resources for new handler

--- music-api/lib/api-stack.ts
+++ music-api/lib/api-stack.ts
@@ -1,70 +1,93 @@
-import * as core from 'aws-cdk-lib';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
-import * as apigateway from 'aws-cdk-lib/aws-apigateway';
-import { LambdaConstruct } from './constructs/lambda';
+import * as core from "aws-cdk-lib"
+import * as lambda from "aws-cdk-lib/aws-lambda"
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb"
+import * as apigateway from "aws-cdk-lib/aws-apigateway"
+import { LambdaConstruct } from "./constructs/lambda"
 
 export class ApiStack extends core.Stack {
   constructor(scope: core.App, id: string, props: any) {
-    super(scope, id);
+    super(scope, id)
 
-    const artistsTable = new dynamodb.Table(this, 'Artists', {
-      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
-      tableName: 'artists',
-    });
+    // Tables
+    const artistsTable = new dynamodb.Table(this, "Artists", {
+      partitionKey: { name: "id", type: dynamodb.AttributeType.STRING },
+      tableName: "artists",
+    })
+    const albumsTable = new dynamodb.Table(this, "Albums", {
+      partitionKey: { name: "id", type: dynamodb.AttributeType.STRING },
+      tableName: "albums",
+    })
 
-    const createArtistLambda = new LambdaConstruct(this, 'CreateArtist', {
-      functionName: 'create-artist-handler',
-      code: lambda.Code.fromAsset('build/apps/create-artist'),
-      handler: 'index.handler',
-    });
-    artistsTable.grantReadWriteData(createArtistLambda.function);
+    // Lambda functions
+    const createArtistLambda = new LambdaConstruct(this, "CreateArtist", {
+      functionName: "create-artist-handler",
+      code: lambda.Code.fromAsset("build/apps/create-artist"),
+      handler: "index.handler",
+    })
+    artistsTable.grantReadWriteData(createArtistLambda.function)
 
-    const getArtistsLambda = new LambdaConstruct(this, 'GetArtists', {
-      functionName: 'get-artists-handler',
-      code: lambda.Code.fromAsset('build/apps/get-artists'),
-      handler: 'index.handler',
-    });
-    artistsTable.grantReadWriteData(getArtistsLambda.function);
+    const createAlbumLambda = new LambdaConstruct(this, "CreateAlbum", {
+      functionName: "create-album-handler",
+      code: lambda.Code.fromAsset("build/apps/create-album"),
+      handler: "index.handler",
+    })
+    albumsTable.grantReadWriteData(createAlbumLambda.function)
 
-    const getArtistByIdLambda = new LambdaConstruct(this, 'GetArtistsById', {
-      functionName: 'get-artist-by-id-handler',
-      code: lambda.Code.fromAsset('build/apps/get-artist-by-id'),
-      handler: 'index.handler',
-    });
-    artistsTable.grantReadWriteData(getArtistByIdLambda.function);
+    const getArtistsLambda = new LambdaConstruct(this, "GetArtists", {
+      functionName: "get-artists-handler",
+      code: lambda.Code.fromAsset("build/apps/get-artists"),
+      handler: "index.handler",
+    })
+    artistsTable.grantReadWriteData(getArtistsLambda.function)
 
-    const searchLambda = new LambdaConstruct(this, 'SearchLambda', {
-      functionName: 'search-handler',
-      code: lambda.Code.fromAsset('build/apps/search'),
-      handler: 'index.handler',
-    });
-    artistsTable.grantReadWriteData(searchLambda.function);
+    const getArtistByIdLambda = new LambdaConstruct(this, "GetArtistsById", {
+      functionName: "get-artist-by-id-handler",
+      code: lambda.Code.fromAsset("build/apps/get-artist-by-id"),
+      handler: "index.handler",
+    })
+    artistsTable.grantReadWriteData(getArtistByIdLambda.function)
+
+    const searchLambda = new LambdaConstruct(this, "SearchLambda", {
+      functionName: "search-handler",
+      code: lambda.Code.fromAsset("build/apps/search"),
+      handler: "index.handler",
+    })
+    artistsTable.grantReadWriteData(searchLambda.function)
 
     // Define the API Gateway resource
-    const api = new apigateway.RestApi(this, 'MusicApi', {
-      restApiName: 'music-api',
-    });
+    const api = new apigateway.RestApi(this, "MusicApi", {
+      restApiName: "music-api",
+    })
 
-    const getArtists = api.root.addResource('artists');
+    // Artists
+    const getArtists = api.root.addResource("artists")
     getArtists.addMethod(
-      'GET',
+      "GET",
       new apigateway.LambdaIntegration(getArtistsLambda.function)
-    );
-    const search = api.root.addResource('search');
-    search.addMethod(
-      'GET',
-      new apigateway.LambdaIntegration(searchLambda.function)
-    );
-    const artist = api.root.addResource('artist');
+    )
+    const artist = api.root.addResource("artist")
     artist.addMethod(
-      'POST',
+      "POST",
       new apigateway.LambdaIntegration(createArtistLambda.function)
-    );
-    const getArtistById = artist.addResource('{id}');
+    )
+    const getArtistById = artist.addResource("{id}")
     getArtistById.addMethod(
-      'GET',
+      "GET",
       new apigateway.LambdaIntegration(getArtistByIdLambda.function)
-    );
+    )
+
+    // Albums
+    const album = api.root.addResource("album")
+    album.addMethod(
+      "POST",
+      new apigateway.LambdaIntegration(createAlbumLambda.function)
+    )
+
+    // Search
+    const search = api.root.addResource("search")
+    search.addMethod(
+      "GET",
+      new apigateway.LambdaIntegration(searchLambda.function)
+    )
   }
 }
