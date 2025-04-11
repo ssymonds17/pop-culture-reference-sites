95b0a6bcfb6e6a1ede91485759975b9a44124c48
Samuel Symonds <samuel.symonds@postcodelottery.co.uk>
2025-04-11 12:00:11 +0100

    Update schema to use arrays of ids for relational properties and add display properties

--- music-api/lambda/schemas/index.ts
+++ music-api/lambda/schemas/index.ts
@@ -1,30 +1,33 @@
-interface Song {
-  id: string; // unique id of the song
-  title: string; // title of the song
-  year: number; // the year the song was released
-  album?: Album; // {optional} the album the song was released on
-  artists: Artist[]; // the artists present on the song
+export interface Song {
+  id: string // unique id of the song
+  title: string // title of the song
+  displayTitle: string // title of the song to be displayed in the client
+  year: number // the year the song was released
+  album?: string // {optional} the id of the album the song was released on
+  artists: string[] // array of ids that represent the artists present on the song
 }
 
-export type Rating = 'GOLD' | 'SILVER' | 'NONE';
+export type Rating = "GOLD" | "SILVER" | "NONE"
 
-interface Album {
-  id: string; // unique id of the album
-  title: string; // title of the album
-  year: number; // the year the album was released
-  artistDisplayName: string; // the display name of the artist of the album
-  artists: Artist[]; // the artists who should be credited with this album
-  songs: Song[]; // the songs on the album
-  rating: Rating; // what is the rating of the album (if applicable)
+export interface Album {
+  id: string // unique id of the album
+  title: string // title of the album
+  displayTitle: string // title of the album to be displayed in the client
+  year: number // the year the album was released
+  artistDisplayName: string // the display name of the artist of the album
+  artists: string[] // the ids of the artists who should be credited with this album
+  songs: string[] // the ids songs on the album
+  rating: Rating // what is the rating of the album (if applicable)
 }
 
-interface Artist {
-  id: string; // unique id of the artist
-  name: string; // the name of the artist
-  albums: Album[]; // the albums of the artist
-  songs: Song[]; // the songs of the artist
-  silverAlbums: number; // how many silver rated albums this artist has
-  goldAlbums: number; // how many gold rated albums this artist has
-  totalSongs: number; // how many songs this artist has
-  totalScore: number; // the total score for this artist
+export interface Artist {
+  id: string // unique id of the artist
+  name: string // the name of the artist
+  displayName: string // title of the artist to be displayed in the client
+  albums: string[] // the ids of the albums of the artist
+  songs: string[] // the ids of the songs of the artist
+  silverAlbums: number // how many silver rated albums this artist has
+  goldAlbums: number // how many gold rated albums this artist has
+  totalSongs: number // how many songs this artist has
+  totalScore: number // the total score for this artist
 }
