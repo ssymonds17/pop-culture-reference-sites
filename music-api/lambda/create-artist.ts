5ee78c9fafbd8b2ca15cb361b2385b1bb615ea28
Samuel Symonds <samuel.symonds@postcodelottery.co.uk>
2025-04-11 12:00:37 +0100

    Add lodash to project

--- music-api/lambda/create-artist.ts
+++ music-api/lambda/create-artist.ts
@@ -1,45 +1,47 @@
-import { PutCommand } from '@aws-sdk/lib-dynamodb';
-import { v7 as uuidV7 } from 'uuid';
-import { createApiResponse } from './utils';
-import { documentClient } from './dynamodb/client';
-import { ARTISTS_TABLE_NAME } from './dynamodb/constants';
+import _ from "lodash"
+import { PutCommand } from "@aws-sdk/lib-dynamodb"
+import { v7 as uuidV7 } from "uuid"
+import { createApiResponse } from "./utils"
+import { Artist } from "./schemas/index"
+import { documentClient, ARTISTS_TABLE_NAME } from "./dynamodb"
 
 const handler = async (event: any) => {
-  const artistName = JSON.parse(event.body).name;
+  const artistName = JSON.parse(event.body).name
   try {
     if (!artistName) {
-      throw new Error('Artist name is required');
+      throw new Error("Artist name is required")
     }
 
-    const artistId = uuidV7();
-    const defaultArtist = {
+    const artistId = uuidV7()
+    const defaultArtist: Artist = {
       id: artistId,
-      name: artistName,
+      name: _.toLower(artistName),
+      displayName: artistName,
       albums: [],
       songs: [],
       silverAlbums: 0,
       goldAlbums: 0,
       totalSongs: 0,
       totalScore: 0,
-    };
+    }
     await documentClient.send(
       new PutCommand({
         TableName: ARTISTS_TABLE_NAME,
         Item: defaultArtist,
-        ConditionExpression: 'attribute_not_exists(id)',
+        ConditionExpression: "attribute_not_exists(id)",
       })
-    );
+    )
 
     return createApiResponse(201, {
       id: artistId,
       artistName,
-      message: 'Successfully created artist',
-    });
+      message: "Successfully created artist",
+    })
   } catch (error) {
     return createApiResponse(502, {
-      message: { message: 'Could not create artist' },
-    });
+      message: { message: "Could not create artist" },
+    })
   }
-};
+}
 
-export { handler };
+export { handler }
