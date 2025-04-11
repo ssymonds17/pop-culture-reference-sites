8e334d4b29928b1244ebf3baf85919da34d0f51f
Samuel Symonds <samuel.symonds@postcodelottery.co.uk>
2025-04-11 12:02:56 +0100

    Update search functionality to use lower case value

--- music-api/lambda/search.ts
+++ music-api/lambda/search.ts
@@ -1,52 +1,55 @@
-import { ScanCommand } from '@aws-sdk/lib-dynamodb';
-import { createApiResponse, logger } from './utils';
-import { documentClient } from './dynamodb/client';
-import { ARTISTS_TABLE_NAME } from './dynamodb/constants';
+import { ScanCommand } from "@aws-sdk/lib-dynamodb"
+import { createApiResponse, logger } from "./utils"
+import { documentClient } from "./dynamodb/client"
+import { ARTISTS_TABLE_NAME } from "./dynamodb/constants"
+import _ from "lodash"
 
 const handler = async (event: any) => {
-  logger.info('event', event);
+  logger.info("event", event)
   try {
-    const searchString = event.queryStringParameters.searchString;
-    logger.info('searchString', searchString);
+    const searchString = event.queryStringParameters.searchString
+    logger.info("searchString", searchString)
 
     if (!searchString) {
-      throw new Error('Missing a searchString parameter');
+      throw new Error("Missing a searchString parameter")
     }
+
+    const searchStringAsLower = _.toLower(searchString)
 
     const params = {
       TableName: ARTISTS_TABLE_NAME,
-      FilterExpression: 'contains(#name, :searchString)',
+      FilterExpression: "contains(#name, :searchString)",
       ExpressionAttributeNames: {
-        '#name': 'name',
+        "#name": "name",
       },
       ExpressionAttributeValues: {
-        ':searchString': searchString,
+        ":searchString": searchStringAsLower,
       },
-    };
+    }
 
-    const result = await documentClient.send(new ScanCommand(params));
+    const result = await documentClient.send(new ScanCommand(params))
 
     if (!result.Items) {
       return createApiResponse(404, {
-        message: 'Could not find any matches',
-      });
+        message: "Could not find any matches",
+      })
     }
 
     return createApiResponse(200, {
       artist: result.Items,
-      message: 'Successfully retrieved matches',
-    });
+      message: "Successfully retrieved matches",
+    })
   } catch (error) {
     if (error instanceof Error) {
       return createApiResponse(404, {
         error: error.message,
-      });
+      })
     }
 
     return createApiResponse(502, {
-      error: 'An unknown error occurred',
-    });
+      error: "An unknown error occurred",
+    })
   }
-};
+}
 
-export { handler };
+export { handler }
