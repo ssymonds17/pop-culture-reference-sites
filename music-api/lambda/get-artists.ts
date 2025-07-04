26e9917ccf8af12c4e684c79c341064ce7038c8e
Samuel Symonds <samuel.symonds@postcodelottery.co.uk>
2025-07-04 16:06:43 +0100

    Update get artists handler to query mongodb instead of dynamodb

--- music-api/lambda/get-artists.ts
+++ music-api/lambda/get-artists.ts
@@ -1,43 +1,20 @@
-import _ from "lodash"
-import {
-  ScanCommand,
-  ScanCommandInput,
-  ScanCommandOutput,
-} from "@aws-sdk/lib-dynamodb"
 import { createApiResponse } from "./utils/api"
-import { documentClient } from "./dynamodb/client"
-import { ARTISTS_TABLE_NAME } from "./dynamodb/constants"
 import { logger } from "./utils"
-import { sortSearchResults } from "./utils/search"
-import { Artist } from "./schemas"
-import { connect } from "http2"
-import { connectToDatabase } from "./mongodb"
+import { connectToDatabase, getArtists } from "./mongodb"
 
 const handler = async () => {
-  const params: ScanCommandInput = {
-    TableName: ARTISTS_TABLE_NAME,
-  }
-
   try {
-    connectToDatabase()
-    const result: ScanCommandOutput = await documentClient.send(
-      new ScanCommand(params)
-    )
+    await connectToDatabase()
+    const artists = await getArtists()
 
-    if (!result.Items) {
+    if (!artists) {
       return createApiResponse(404, {
         message: "Could not find artists",
       })
     }
 
-    const sortedItems = sortSearchResults(
-      result.Items as Artist[],
-      "artist"
-    ) as Artist[]
-    const filteredItems = _.take(sortedItems, 100)
-
     return createApiResponse(200, {
-      artists: filteredItems,
+      artists,
       message: "Successfully retrieved artists",
     })
   } catch (error) {
