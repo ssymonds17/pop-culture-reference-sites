04d8f9332e067bf45051349bb049e053b811cf58
Samuel Symonds <samuel.symonds@postcodelottery.co.uk>
2025-08-01 14:25:39 +0100

    Correctly return albums in rating order first

--- music-api/lambda/mongodb/services/albums.ts
+++ music-api/lambda/mongodb/services/albums.ts
@@ -1,14 +1,33 @@
-import Album, { AlbumData } from "../models/album"
+import Album, { AlbumData, Rating } from "../models/album"
 
 export const createAlbum = async (albumData: AlbumData) => {
   return Album.create(albumData)
 }
 
 export const getAlbums = async () => {
-  return Album.find({}, null)
-    .sort({ artistDisplayName: 1, year: 1, rating: 1 })
+  const gold = await Album.find({ rating: Rating.GOLD })
+    .sort({
+      artistDisplayName: 1,
+      year: 1,
+    })
     .limit(100)
     .exec()
+  const silver = await Album.find({ rating: Rating.SILVER })
+    .sort({
+      artistDisplayName: 1,
+      year: 1,
+    })
+    .limit(100)
+    .exec()
+  const none = await Album.find({ rating: Rating.NONE })
+    .sort({
+      artistDisplayName: 1,
+      year: 1,
+    })
+    .limit(100)
+    .exec()
+
+  return [...gold, ...silver, ...none]
 }
 
 export const getAlbumById = async (id: string) => {
