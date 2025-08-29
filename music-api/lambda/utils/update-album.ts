a75697028ea0bdb19a8fd7afcd0ec4a32a5475bb
Samuel Symonds <samuel.symonds@postcodelottery.co.uk>
2025-08-29 11:09:58 +0100

    Create update album utils to ensure artist is properly updated when album rating changes

---
+++ music-api/lambda/utils/update-album.ts
@@ -0,0 +1,52 @@
+import { ArtistDocument } from "../mongodb/models/artist"
+import { Rating } from "../mongodb/models/album"
+import { updateScoreBasedOnAlbumRatingUpdate } from "./score"
+
+export const updateAlbumTotal = (
+  ratingCategory: Rating,
+  albumTotal: number,
+  oldRating: Rating,
+  newRating: Rating
+) => {
+  if (oldRating === ratingCategory) {
+    return albumTotal - 1
+  }
+  if (newRating === ratingCategory) {
+    return albumTotal + 1
+  }
+  return albumTotal
+}
+
+export const updateAssociatedArtists = async (
+  artists: ArtistDocument[],
+  albumId: string,
+  oldRating: Rating,
+  newRating: Rating
+) => {
+  for (const artist of artists) {
+    const newArtistAlbums = [...artist.albums, albumId]
+
+    // Update the artist document
+    artist.albums = newArtistAlbums
+    artist.goldAlbums = updateAlbumTotal(
+      Rating.GOLD,
+      artist.goldAlbums,
+      oldRating,
+      newRating
+    )
+    artist.silverAlbums = updateAlbumTotal(
+      Rating.SILVER,
+      artist.silverAlbums,
+      oldRating,
+      newRating
+    )
+    artist.totalScore = updateScoreBasedOnAlbumRatingUpdate(
+      artist.totalScore,
+      oldRating,
+      newRating
+    )
+
+    // Save the updated artist
+    await artist.save()
+  }
+}
